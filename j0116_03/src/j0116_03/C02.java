3535b520d6c7c00f1730ac534d855450eb09cca2
m.vagga <m.vagga@gmail.com>
2026-01-19 09:35:21 +0900

    test

--- j0116_03/src/j0116_03/C02.java
+++ j0116_03/src/j0116_03/C02.java
@@ -2,6 +2,21 @@ package j0116_03;
 
 public class C02 {
 	public static void main(String[] args) {
-		System.out.println("bbb");
+		
+		
+//		int[] arr = new int[]{1,2,3};
+		int[] arr = {10,20,30};
+		
+		for( int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		};
+		
+		for (int num : arr) {
+			System.out.println(num);
+		}
+		
+		int [][] arr2 = new int[2][3];
+		
+		
 	}
 }
